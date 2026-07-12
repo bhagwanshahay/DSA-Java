e406e5dbb4673b450c393a8e7f588f35efaf026d
Bhagwan Shahay <bhagwanshahay.dev@gmail.com>
2026-07-13 00:53:57 +0530

    basicMaths practiced

--- Java-Basics/basicMaths.java
+++ Java-Basics/basicMaths.java
@@ -2,13 +2,39 @@ public class basicMaths {
     static void printDigits(int number){
         while(number != 0) {
             int lastDigit = number % 10;
-            System.out.println(lastDigit);
+            System.out.println(  lastDigit);
             number = number / 10;
         }
+    }
+    static void countDigit(int number){
+        
+        int count = 0 ;
+        
+        while( number != 0) {
+            int lastDigit = number % 10 ;
+            count += 1 ;
+            number = number /10 ;
+        }
+        System.out.println("This is the count of digits : " + count);
+    }
+
+    static void sumDigit(int number){
+        
+        int sum = 0 ;
+        
+        while( number != 0) {
+            int lastDigit = number % 10 ;
+            sum += lastDigit;
+            number = number /10 ;
+        }
+        System.out.println("this is the Sum of digits : "+ sum);
     }
     public static void main(String[] args) {
         int number = 53127;
         printDigits(number);
+        countDigit(number);
+        sumDigit(number);
+
 
     }
 }
