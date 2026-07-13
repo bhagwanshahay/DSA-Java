72b7043b86494559893388e62dd6b1d785418d2c
Bhagwan Shahay <bhagwanshahay.dev@gmail.com>
2026-07-14 01:34:44 +0530

    suffleArray Leetcode problem solved

--- Arrays/suffleArray.java
+++ Arrays/suffleArray.java
@@ -1,6 +1,6 @@
 package Arrays;
-
-public class suffleArray {
+import java.util.Arrays;
+    public class suffleArray {
     static int[] shuffle(int[] nums, int n) {
         int finalArray[] = new int[nums.length];
         int firstGroup[] = new int[n];
@@ -10,15 +10,19 @@ public class suffleArray {
             if (i < n) {
                firstGroup[i] = nums[i];
            } else {
-            secondGroup[i] = nums[i-1];
+                secondGroup[i-n] = nums[i];
            }
-
-
         }
-        return firstGroup;
+        for (int i = 0; i < firstGroup.length; i++) {
+                finalArray[i * 2] = firstGroup[i];
+                finalArray[i * 2 + 1] = secondGroup[i];
+                
+        }
+        return finalArray;
+        
     }
     public static void main(String[] args) {
         int nums[] = {2,5,1,3,4,7};
-        shuffle(nums,3);
+        System.out.println(Arrays.toString(shuffle(nums,3)));
     }
 }
