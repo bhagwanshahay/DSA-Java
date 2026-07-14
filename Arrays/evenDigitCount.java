99d9d9a4c03c31561a3f8b8132514298635e2ce5
Bhagwan Shahay <bhagwanshahay.dev@gmail.com>
2026-07-14 23:32:49 +0530

    Find Number with Even Number of Digits leetcode problem solved

---
+++ Arrays/evenDigitCount.java
@@ -0,0 +1,39 @@
+package Arrays;
+
+public class evenDigitCount {
+    static int findNumbers(int[] nums) {
+            
+            int ans = 0; 
+            int number ;
+
+            for (int i = 0 ; i < nums.length ; i++) {
+                number = nums[i];
+                int digitCount = 0; 
+
+                if (number == 0) {
+                    digitCount = 1;
+                } else {
+
+                while (number > 0) {
+                
+                    number = number/10;
+                 digitCount ++;
+                 
+        } 
+    }
+
+        if (digitCount % 2 == 0 ) {
+            
+            ans ++;
+
+        }
+            }
+
+        return ans;
+    }
+    public static void main(String[] args) {
+        int nums[] = {12,345,2,6,7896};
+        // int nums[] = {0};
+        System.out.println(findNumbers(nums));
+    }
+}
