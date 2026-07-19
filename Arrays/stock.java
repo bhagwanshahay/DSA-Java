a7e50935dd8086578f6916696d40741e6b1d3689
Bhagwan Shahay <bhagwanshahay.dev@gmail.com>
2026-07-19 22:46:14 +0530

    error solved

--- Arrays/stock.java
+++ Arrays/stock.java
@@ -21,6 +21,7 @@ public class Stock {
     public static void main(String[] args) {
         int[] prices = {7,1,5,3,6,4};
         System.out.println(maxProfit(prices));
+        System.out.println("Git is working ?");
     }
     
 }
