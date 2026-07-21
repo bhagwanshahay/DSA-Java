59b3879162ad6bd3196e49904470fb78f4ceda3b
Bhagwan Shahay <bhagwanshahay.dev@gmail.com>
2026-07-21 22:44:37 +0530

    Solve github contribution problem

--- Arrays/RemoveDuplicates.java
+++ Arrays/RemoveDuplicates.java
@@ -14,5 +14,6 @@ public class RemoveDuplicates {
     public static void main(String[] args) {
         int nums[] = {1,1,2};
         System.out.println(removeDuplicates(nums));
+        System.out.println(removeDuplicates(nums));
     }
 }
