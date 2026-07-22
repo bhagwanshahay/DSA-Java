df3f5d2834ed294a26bfa9f646ba4f72aa0a4a33
Bhagwan Shahay <bhagwanshahy.dev@gmail.com>
2026-07-22 22:15:29 +0530

    StudentGrade aded

--- Arrays/RemoveDuplicates.java
+++ Arrays/RemoveDuplicates.java
@@ -17,11 +17,38 @@ public class RemoveDuplicates {
             int input = sc.nextInt();
             System.out.println(input);
         }
+        static void studentGrade(int marks) {
+            if (marks >= 90) {
+                System.out.println("Grade A");
+            } else {
+                if (marks >= 70) {
+                    System.out.println("Grade B");
+                } else {
+                    if ( marks >= 50) {
+                        System.out.println("Grade C");
+                    } else {
+                        if (marks >= 35) {
+                            System.out.println("Grade D");
+                        } else {
+                            System.out.println("Fail");
+                        }
+                    }
+
+                }
+            }
+    }
     public static void main(String[] args) {
-        int nums[] = {1,1,2};
-        System.out.println(removeDuplicates(nums));
-        System.out.println(removeDuplicates(nums));
+        // int nums[] = {1,1,2};
+        // System.out.println(removeDuplicates(nums));
+        // System.out.println(removeDuplicates(nums));
+
+
         // Scanner sc = new Scanner(System.in);
         // printNumber(sc);
+
+        int marks = 95;
+        studentGrade(marks);
+
+
     }
 }
