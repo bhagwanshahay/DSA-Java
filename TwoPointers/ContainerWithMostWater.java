863a1dd9221ef9bdbc250480f7113d371a5bee71
Bhagwan Shahay <bhagwanshahy.dev@gmail.com>
2026-07-26 22:24:33 +0530

    Container With Most Water Leetcode  Problem solved

--- TwoPointers/ContainerWithMostWater.java
+++ TwoPointers/ContainerWithMostWater.java
@@ -1,7 +1,25 @@
 package TwoPointers;
 
 public class ContainerWithMostWater {
+    static int maxArea(int[] height) {
+        int left = 0;
+        int right = height.length - 1;
+        int maxArea = 0;
+
+        while ( left < right  ) {
+               maxArea = Math.max(maxArea, (right - left) * Math.min(height[left], height[right]));
+
+                    if ( height[left] < height[right] ) {
+                        left ++;
+                    } else {
+                        right --;
+                    }
+                 
+        }
+        return maxArea;
+    }
     public static void main(String[] args) {
-        
+        int[] height = {1,8,6,2,5,4,8,3,7};
+        System.out.println(maxArea(height));
     }
 }
