fd5d36d4afa2b53234926ab3eb4add1056ed1dee
Bhagwan Shahay <bhagwanshahay.dev@gmail.com>
2026-07-27 21:13:53 +0530

    Java Collection and Framework Folder added

---
+++ Java Collection and Framework/arrayList.java
@@ -0,0 +1,32 @@
+package Java Collection and Framework;
+
+import java.util.Collection;
+import java.util.ArrayList;
+import java.util.List;
+
+public class arrayList {
+    public static void main(String[] args) {
+        
+        ArrayList<Integer> array =  new ArrayList<>();
+        // List<Integer> list = new ArrayList<>();
+        // Collection<Integer> collection = new ArrayList<>();
+
+
+
+
+        array.add(10);
+        array.add(20);
+        array.add(30);
+        array.add(40);
+        array.add(50);
+        array.add(60);
+        array.add(70);
+        array.add(80);
+
+        System.out.println(array);
+
+
+
+
+    }
+}
