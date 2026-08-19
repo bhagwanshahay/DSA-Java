5650fa47a9e730d748cea4feb8f3b4053d8c3f17
Bhagwan Shahay <bhagwanshahay.dev@gmail.com>
2026-08-20 00:36:16 +0530

    Union Of Two Sorted Arrays Solved with time complaxity of O(m*n)

--- Arrays/UnionOfTwoSortedArrays.java
+++ Arrays/UnionOfTwoSortedArrays.java
@@ -1,28 +1,70 @@
 package Arrays;
+import java.util.ArrayList;
 import java.util.HashSet;
 
 public class UnionOfTwoSortedArrays {
    
     static int[] unionArray(int[] nums1, int[] nums2) {
-        HashSet<Integer> set = new HashSet<>();
-
-        for (int i = 0; i < nums1.length; i++) {
-            set.add(nums1[i]);
-        }
-
-        for (int j = 0; j < nums2.length; j++) {
-            set.add(nums2[j]);
-        }
-
-        int[] result = new int[set.size()];
 
         int i = 0;
-        for (int value : set) {
-            result[i] = value;
-            i++;
+        int j = 0;
+        int current = 0;
+
+        ArrayList<Integer> result = new ArrayList<>();
+
+       
+        while (i < nums1.length && j < nums2.length) {
+
+            if (nums1[i] < nums2[j]) {
+                current = nums1[i];
+                i++;
+
+            } else if (nums1[i] > nums2[j]) {
+                current = nums2[j];
+                j++;
+
+            } else {
+                current = nums1[i];
+                i++;
+                j++;
+            }
+
+            if (result.isEmpty() ||
+                result.get(result.size() - 1) != current) {
+                result.add(current);
+            }
         }
 
-        return result;
+       
+        while (i < nums1.length) {
+            current = nums1[i];
+            i++;
+
+            if (result.isEmpty() ||
+                result.get(result.size() - 1) != current) {
+                result.add(current);
+            }
+        }
+
+       
+        while (j < nums2.length) {
+            current = nums2[j];
+            j++;
+
+            if (result.isEmpty() ||
+                result.get(result.size() - 1) != current) {
+                result.add(current);
+            }
+        }
+
+       
+        int[] ans = new int[result.size()];
+
+        for (int k = 0; k < result.size(); k++) {
+            ans[k] = result.get(k);
+        }
+
+        return ans;
     }
     public static void main(String[] args) {
         int nums1[] = {1,2,3,4,5};
