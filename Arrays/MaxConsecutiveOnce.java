8196277330420917574e07188cb6d74624f37950
Bhagwan Shahay <bhagwanshahay.dev@gmail.com>
2026-08-21 00:54:53 +0530

    Max consecutive Once Leetcode 485 Problem solved

---
+++ Arrays/MaxConsecutiveOnce.java
@@ -0,0 +1,23 @@
+package Arrays;
+import java.util.*;
+public class MaxConsecutiveOnce {
+    static int findMaxConsecutiveOnes(int[] nums) {
+        int count = 0;
+        int max = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+                if (nums[i] == 1) {
+                    count ++;
+                } else {
+                    count = 0;
+                }
+
+             max = Math.max(count,max);   
+        }
+        return max;
+    }
+    public static void main(String[] args) {
+        int nums[] = {1,1,0,1,1,1};
+        System.out.println(findMaxConsecutiveOnes(nums));
+    }
+}
