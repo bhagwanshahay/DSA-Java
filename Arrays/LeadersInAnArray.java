7743e280b6bdc22dc8cf9dc9ce245e812e403d1b
Bhagwan Shahay <bhagwanshahay.dev@gmail.com>
2026-08-26 22:32:54 +0530

    Leaders In An Array Optimized solution

--- Arrays/LeadersInAnArray.java
+++ Arrays/LeadersInAnArray.java
@@ -23,6 +23,6 @@ public class LeadersInAnArray {
     public static void main(String[] args) {
         int nums[] = {1, 2, 5, 3, 1, 2};
         System.out.println(leaders(nums));
-        
+        //it's containing the method from right side which is more optimal or optimized then we do normally brute force which is big o of n square .
     }
 }
